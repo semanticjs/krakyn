a97d1816ce97b9d28e48a1be3f911e84a596bb75
xchanin <shannonwbruns@gmail.com>
2021-11-10 09:46:14 -0700

    copy files over from krakyn02

--- src/utils/menu-templates.utils.ts
+++ src/utils/menu-templates.utils.ts
@@ -5,7 +5,8 @@ export class MenuTemplatesUtils {
 
     public static Render(el: HTMLElement, menuTemplates: Array<MenuTemplateModel>): void {
         
-        menuTemplates.forEach((item: MenuTemplateModel) => {
+        for (let item of menuTemplates) {
+
             const container: HTMLElement = document.createElement('div'); 
             container.setAttribute('draggable', String(item.Draggable));
             container.setAttribute('data-node', item.DataNode);
@@ -19,30 +20,11 @@ export class MenuTemplatesUtils {
             span.innerHTML = item.Label;
 
             container.appendChild(img);
-            img.appendChild(span);
+            // img.appendChild(span);
+            container.appendChild(span);
     
             el.appendChild(container);
-        })
-
-        // for (let item of menuTemplates) {
-
-        //     const container: HTMLElement = document.createElement('div'); 
-        //     container.setAttribute('draggable', String(item.Draggable));
-        //     container.setAttribute('data-node', item.DataNode);
-        //     container.addEventListener('dragstart', item.DragAction);
-        //     container.classList.add(...item.ClassList);
-
-        //     const img: HTMLElement = document.createElement('i');
-        //     img.classList.add(...item.IconClassList);
-
-        //     const span: HTMLElement = document.createElement('span');
-        //     span.innerHTML = item.Label;
-
-        //     container.appendChild(img);
-        //     img.appendChild(span);
-    
-        //     el.appendChild(container);
-        // }
+        }
     }
 }
 
