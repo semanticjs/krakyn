e87aa6f888af5d92a881505c42cc9588d341648f
xchanin <shannonwbruns@gmail.com>
2022-01-31 11:58:54 -0700

    changes to data setup

--- src/utils/constants.utils.ts
+++ src/utils/constants.utils.ts
@@ -4,118 +4,7 @@ import { NodeTemplates } from '../templates/node-templates.js';
 import { NodeModel } from '../models/nodes/node.model.js';
 import { EnterpriseAsCode } from '@semanticjs/common';
 
-// import {
-//   TestNapkinIDEConfig,
-//   TestNapkinIDEFlow,
-//   NapkinIDENode,
-//   NapkinIDEEdge,
-// } from '@semanticjs/napkin-ide';
 
-export const ExternalDataTest: any = {
-  
-  Nodes: [
-    {
-      ID: '1AA', // ids that aren't just numbers don't work - also, the position
-                // of the node, if not set in the config, is applied by multiplying
-                // the id by X, so large ids put the node off screen
-      Type: 'request',
-      ClassList: ['persisted-class'],
-      Data: {},
-    },
-    {
-      ID: '2',
-      Type: 'project',
-      Data: {
-        Name: 'IoT Ensemble',
-        Host: 'www.iot-ensemble.com',
-      },
-    },
-  ],
-  Edges: [
-    {
-      ID: '1AA',
-      Outputs: {
-        output_1: {
-          Connections: [],
-        }
-      }
-    },
-    {
-      ID: '2',
-      Inputs: {
-        input_1: {
-          Connections: [
-            {
-              node: 'AA',
-              input: 'output_1',
-            },
-          ],
-        }
-      },
-      Outputs: {
-        output_1: {
-          Connections: [],
-        }
-      }
-    }
-  ],
-}
-
-export const ExternalDataConfig: any = {
-
-    "Name": "shannon.bruns@fathym.com Enterprise",
-    "ID": "6e8f3051-6b07-430a-b987-c68692530131",
-    Nodes: [
-        {
-            "ID": "sys-1",
-            "Type": "request",
-            "ClassList": [],
-            "Data": {}
-        },
-        {
-            "Type": "project",
-            "ID": "project-1a9f69fb-50ce-46b9-a934-a5191a0959cc",
-            "Data": {
-                "Name": "Docusaurus - Documentation - Open Source",
-                "Hosts": [
-                    "b206e58771d7-shared.lowcodeunit.com"
-                ]
-            }
-        }
-    ],
-    Edges: [
-        {
-            "ID": "sys-edge-2",
-            "NodeInID": "sys-1",
-            "NodeOutID": "project-1a9f69fb-50ce-46b9-a934-a5191a0959cc",
-            Outputs: {
-              output_1: {
-                Connections: [],
-              }
-            },
-        },
-        {
-            "ID": "sys-edge-3",
-            "NodeInID": "project-1a9f69fb-50ce-46b9-a934-a5191a0959cc",
-            "NodeOutID": "route-filter-path-/.*-0",
-            Inputs: {
-              input_1: {
-                Connections: [
-                  {
-                    node: 'sys-1',
-                    input: 'output_1',
-                  },
-                ],
-              }
-            },
-            Outputs: {
-              output_1: {
-                Connections: [],
-              }
-            },
-        }
-    ]
-}
 export const TestNapkinIDEConfig: any = {
   'NodeTypes': {
     request: {
@@ -143,8 +32,17 @@ export const TestNapkinIDEConfig: any = {
   },
 };
 
-export const TestNapkinIDEFlow: any = {
-  Nodes: [
+
+export class ConstantUtils {
+
+  static count: number = 0;
+
+  protected static getProperty<T, K extends keyof T>(o: T, propertyName: K): T[K] {
+    return o[propertyName];
+  }
+
+  public static ORIGINAL_TEST_DATA: any = {
+   Nodes: [
     {
       ID: '1',
       Type: 'request',
@@ -157,7 +55,7 @@ export const TestNapkinIDEFlow: any = {
       Data: {
         Name: 'IoT Ensemble',
         Host: 'www.iot-ensemble.com',
-      },
+      }
     },
     {
       ID: '3',
@@ -174,9 +72,9 @@ export const TestNapkinIDEFlow: any = {
         Package: '@iot-ensemble/public-web',
         Version: 'latest',
       },
-    },
-  ],
-  Edges: [
+    }
+   ],
+   Edges: [
     {
       ID: '1',
       Outputs: {
@@ -248,159 +146,143 @@ export const TestNapkinIDEFlow: any = {
       NodeInID: '2',
       NodeOutID: '3',
     }
+   ]
+  };
+
+  public static EXTERNAL_DATA_TEST: any = {
+    "Name": "shannon.bruns@fathym.com Enterprise",
+    "ID": "6e8f3051-6b07-430a-b987-c68692530131",
+    Nodes: [
+        {
+            "ID": "sys-1",
+            "Type": "request",
+            "ClassList": [],
+            "Data": {}
+        },
+        {
+            "Type": "project",
+            "ID": "project-1a9f69fb-50ce-46b9-a934-a5191a0959cc",
+            "Data": {
+                "Name": "Docusaurus - Documentation - Open Source",
+                "Hosts": [
+                    "b206e58771d7-shared.lowcodeunit.com"
+                ]
+            }
+        }
+    ],
+    Edges: [
+        {
+            "ID": "sys-edge-2",
+            "NodeInID": "sys-1",
+            "NodeOutID": "project-1a9f69fb-50ce-46b9-a934-a5191a0959cc"
+        },
+        {
+            "ID": "sys-edge-3",
+            "NodeInID": "project-1a9f69fb-50ce-46b9-a934-a5191a0959cc",
+            "NodeOutID": "route-filter-path-/.*-0"
+        }
+    ]
+  }
+
+  public static DATA_TEST: any = {
+      
+  Nodes: [
+    {
+      ID: '11a', 
+      Type: 'request',
+      ClassList: ['persisted-class'],
+      Data: {},
+    },
+    {
+      ID: '2',
+      Type: 'project',
+      Data: {
+        Name: 'IoT Ensemble',
+        Host: 'www.iot-ensemble.com',
+      },
+    },
   ],
-};
-
-export class ConstantUtils {
-
-  static count: number = 0;
-
-  protected static getProperty<T, K extends keyof T>(o: T, propertyName: K): T[K] {
-    return o[propertyName];
+  Edges: [
+    {
+      ID: '11a',
+      Outputs: {
+        output_1: {
+          Connections: [],
+        }
+      }
+    },
+    {
+      ID: '2',
+      Inputs: {
+        input_1: {
+          Connections: [
+            {
+              node: '11a',
+              input: 'output_1',
+            },
+          ],
+        }
+      },
+      Outputs: {
+        output_1: {
+          Connections: [],
+        }
+      }
+    }
+  ],
   }
 
-  /**
-   * My test - shannon
-<<<<<<< HEAD
-   * 
-   * @returns 
-=======
-   * @returns
->>>>>>> f5fa06b3992f6283d8dd5bda1c44264605467da9
-   */
-  public static DataToImport(): EnterpriseAsCode {
-    let code: EnterpriseAsCode;
-    code.Enterprise.Name = 'NapkinIDE';
-    code.EnterpriseLookup = '1';
+  public static MapData(moduleName: string, data: any): any {
 
+    return {
+      Module: moduleName,
+      Data: data.Nodes!.map((node: any, index: number) => {
+        const config = TestNapkinIDEConfig.NodeTypes![<string>node.Type];
 
-    return code;
-  }
+        const inputs = data.Edges?.filter(
+          (edge: any) => edge.ID === node.ID
+        ).map((edge: any) => {
 
-  public static NAPKIN_IDE_MODULE_DATA: DataFlowDataModel = {
-    Module: 'NapkinIDE',
-    Data: TestNapkinIDEFlow.Nodes!.map((node: any) => {
-      const config = TestNapkinIDEConfig.NodeTypes![<string>node.Type];
-
-      const inputs = TestNapkinIDEFlow.Edges?.filter(
-        (edge: any) => edge.ID === node.ID
-      ).map((edge: any) => {
-
-        return edge.Inputs;
-      });
-
-      const outputs = TestNapkinIDEFlow.Edges?.filter(
-        (edge: any) => edge.ID === node.ID
-      ).map((edge: any) => {
-
-        return edge.Outputs
-      });
-
-      return ConstantUtils.newModel(config, node, inputs, outputs);
-    }),
-  };
-
-  public static HOME_MODULE_DATA: DataFlowDataModel = {
-    Module: 'Home',
-    Data: TestNapkinIDEFlow.Nodes!.map((node: any) => {
-      const config = TestNapkinIDEConfig.NodeTypes![<string>node.Type];
-
-      const inputs = TestNapkinIDEFlow.Edges?.filter(
-        (edge: any) => edge.ID === node.ID
-      ).map((edge: any) => {
-
-        return edge.Inputs;
-      });
-
-      const outputs = TestNapkinIDEFlow.Edges?.filter(
-        (edge: any) => edge.ID === node.ID
-      ).map((edge: any) => {
-
-        return edge.Outputs
-      });
-
-      return ConstantUtils.newModel(config, node, inputs, outputs);
-    }),
-  };
-
-  /**
-   * Testing new data structure for external data
-   *
-   * @returns data object
-   */
-  public static EXTERNAL_MODULE_DATA: any = {
-    Module: 'ExternalData',
-    Data: ExternalDataConfig.Nodes!.map((node: any) => {
-      const config = TestNapkinIDEConfig.NodeTypes![<string>node.Type];
-
-      const inputs = TestNapkinIDEFlow.Edges?.filter(
-        (edge: any) => edge.ID === node.ID
-      ).map((edge: any) => {
-
-        if (!edge.Inputs) {
-
-          edge.Inputs = {
-            input_1: {
-              Connections: [
-                {
-                  node: edge.ID,
-                  input: 'output_1'
-                }
-              ]
+          if (!edge.Inputs) {
+            edge.Inputs = {
+              input_1: {
+                Connections: [
+                  {
+                    node: edge.ID,
+                    input: 'output_1'
+                  }
+                ]
+              }
             }
           }
-        }
 
-        return edge.Inputs;
-      });
+          return edge.Inputs;
+        });
 
-      const outputs = TestNapkinIDEFlow.Edges?.filter(
-        (edge: any) => edge.ID === node.ID
-      ).map((edge: any) => {
+        const outputs = data.Edges?.filter(
+          (edge: any) => edge.ID === node.ID
+        ).map((edge: any) => {
 
-        if (!edge.Outputs) {
-          
-          edge.Outputs = {
-            output_1: {
-              Connections: [
-                {
-                  node: edge.ID,
-                  output: 'input_1'
-                }
-              ]
+          if (!edge.Outputs) {
+            edge.Outputs = {
+              output_1: {
+                Connections: [
+                  {
+                    node: edge.ID,
+                    output: 'input_1'
+                  }
+                ]
+              }
             }
           }
-        }
 
-        return edge.Outputs
-      });
+          return edge.Outputs
+        });
 
-      return ConstantUtils.newModel(config, node, inputs, outputs);
-    }),
+        return ConstantUtils.newModel(config, node, inputs, outputs, index);
+      })
+    }
   }
-
-  public static EXTERNAL_TEST_DATA: DataFlowDataModel = {
-    Module: 'ExternalTestData',
-    Data: ExternalDataTest.Nodes!.map((node: any) => {
-      const config = TestNapkinIDEConfig.NodeTypes![<string>node.Type];
-
-      const inputs = ExternalDataTest.Edges?.filter(
-        (edge: any) => edge.ID === node.ID
-      ).map((edge: any) => {
-
-        return edge.Inputs;
-      });
-
-      const outputs = ExternalDataTest.Edges?.filter(
-        (edge: any) => edge.ID === node.ID
-      ).map((edge: any) => {
-
-        return edge.Outputs
-      });
-
-      return ConstantUtils.newModel(config, node, inputs, outputs);
-    }),
-  };
 
   /**
    * Return a new model
@@ -411,7 +293,13 @@ export class ConstantUtils {
    * @param outputs
    * @returns
    */
-  protected static newModel(config: any, node: any, inputs: any, outputs: any): NodeModel {
+  protected static newModel(
+    config: any, 
+    node: any, 
+    inputs: any, 
+    outputs: any,
+    index: number
+    ): NodeModel {
 
     return <NodeModel>{
       AllowedInputTypes: config.AllowedInputTypes,
@@ -426,8 +314,8 @@ export class ConstantUtils {
       NumOfInputs: config.InputCountLimit,
       NumOfOutputs: config.OutputCountLimit,
       Outputs: outputs[0],
-      PosY: node.PositionY || node.ID * 150,
-      PosX: node.PositionX || node.ID * 100,
+      PosY: node.PositionY || index * 150,
+      PosX: node.PositionX || index * 100,
       TypeNode: false,
     };
   }
