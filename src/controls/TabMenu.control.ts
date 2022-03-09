cd8f9f2b6e6b503c5fb6430244ddc92da5f23ad3
xchanin <shannonwbruns@gmail.com>
2022-03-09 16:52:38 -0700

    remove TabItems check

--- src/controls/TabMenu.control.ts
+++ src/controls/TabMenu.control.ts
@@ -65,7 +65,7 @@ export class TabMenuControl extends LitElement {
         `
        
 
-        <div class="tab-menu" is="dom-if" if="{{this.TabItems}}>
+        <div class="tab-menu">
             <ul>
                 ${ this.TabItems.map((item: ModuleMenuModel) => {
                     return html 
